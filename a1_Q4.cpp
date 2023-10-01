76f03e749d0666a512ad2c26af17b6d289fe5987
pariig <142706639+pariig@users.noreply.github.com>
2023-10-01 18:21:28 +0530

    Create a1_Q4.cpp

---
+++ a1_Q4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int low,high,j;
+  cout<<'enter the low limit';
+  cin<<low;
+  cin<<upper;
+  for(i=low;i<=high;i++)
+{  cnt=0
+    for(j=1;j<=i;j++)
+{    if(i%j==0)
+      cnt++
+        }
+    if (cnt==2)
+      cout<<i<<endl;
+}
+}
